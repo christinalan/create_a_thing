73d17f3c54f79698932e9e217fe939890149739c
Christina Lan <christinalan@Christinas-MBP.home>
2020-10-23 17:19:57 -0400

    added a filter and p5 canvas

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let meme;
 let memeName;
 let memeImage;
+let memeURL;
+let p5Image;
 
+//loads initial meme
 window.addEventListener("load", function() {
 fetch("https://api.imgflip.com/get_memes")
 .then(response => response.json())
@@ -17,9 +20,14 @@ fetch("https://api.imgflip.com/get_memes")
     let memeImage = document.getElementById("meme_img")
     memeImage.src = memeData[randomMeme].url;
 
+    memeURL = memeData[randomMeme].url
+    p5Image = loadImage(memeURL)
+    console.log(p5Image);
+
     })
 });
 
+//refreshes the meme with the meme button
 let memeButton = document.getElementById("button");
 memeButton.addEventListener("click", function() {
 
@@ -37,5 +45,19 @@ memeButton.addEventListener("click", function() {
     memeImage = document.getElementById("meme_img")
     memeImage.src = memeData[randomMeme].url;
 
+    memeURL = memeData[randomMeme].url
+    p5Image = loadImage(memeURL)
     })
 })
+
+//p5 to distort images?
+function setup() {
+    createCanvas(400, 400);
+    background('black');
+}
+
+
+function draw() {
+    image(p5Image, 0, 0)
+    filter(INVERT);
+}
